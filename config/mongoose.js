077a3abf1f8e6cb1b29f01255f67dc411943f725
unknown <devanshugupta935@gmail.com>
2023-09-21 19:46:33 +0530

    Added layouts and partials and configured connectivity to mongo DB

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+//require the library
+const mongoose = require('mongoose');
+
+// connect to database
+mongoose.connect('mongodb://127.0.0.1:27017/codeial_development');
+
+// acquire the connection (to check if it is successful)
+const db = mongoose.connection;
+
+// error
+db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
+
+// up and running then print the message
+db.once('open', function(){
+    console.log('Connected to Database :: MongoDB');
+});
+
+
+module.exports = db;
