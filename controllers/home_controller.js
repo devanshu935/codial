ab8b27c4c79623a0f3780d6dc8eeb1778409ed63
unknown <devanshugupta935@gmail.com>
2023-10-08 16:24:16 +0530

    Added feature to make comments on posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,6 @@
 const Post = require('../models/post');
 
 module.exports.home = function(req, res){
-    console.log(req.cookies);
     // Post.find({}).then((posts) => {
     //     return res.render('home', {
     //         title: "Codial | Home",
@@ -9,11 +8,24 @@ module.exports.home = function(req, res){
     //     });
     // });
 
-    Post.find({}).populate('user').then((posts) => {
-        return res.render('home', {
-            title: "Codial | Home",
-            posts: posts 
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        } 
+    })
+    .then(posts => {
+        res.render('home', {
+          title: "Codial | Home",
+          posts: posts 
         });
+      })
+    .catch(err => {
+    // Handle any errors here
+    console.error(err);
     });
 }
+
 //module.exports.actionName = function(req, res){}
