0eb9a989df73b64967e04c8d4e47fd3c035bb3a2
unknown <devanshugupta935@gmail.com>
2023-10-10 13:10:27 +0530

    Added feature to delete the comments made on a post

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,5 @@
-const Comment = require('../models/comment');
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     Post.findById(req.body.post).then((post) => {
@@ -14,5 +14,20 @@ module.exports.create = function(req, res){
         }).catch((err) => {
             console.log(err);
         });
+    });
+}
+
+module.exports.destroy = function(req, res){
+    Comment.findById(req.params.id).then((comment) => {
+        if(comment.user == req.user.id){
+            let postId = comment.post;
+            Comment.deleteOne({_id: comment.id}).then(() => {
+                Post.findByIdAndUpdate(postId, {$pull: {comments: req.params.id}}).then(() => {
+                    return res.redirect('back');
+                });
+            });
+        }
+    }).catch((err) => {
+        console.log(err);
     });
 }
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment')
 
 module.exports.create = function(req, res){
     Post.create({
@@ -10,3 +11,17 @@ module.exports.create = function(req, res){
         console.log(err);
     });
 }
+
+module.exports.destroy = function(req, res){
+    Post.findById(req.params.id).then((post) => {
+        if(post.user == req.user.id){
+            Post.deleteOne({_id: post.id}).then(() => {
+                Comment.deleteMany({post: req.params.id}).then(() =>{
+                    return res.redirect('back');
+                });
+            });
+        }
+    }).catch((err) => {
+        console.log(err);
+    });
+} 
